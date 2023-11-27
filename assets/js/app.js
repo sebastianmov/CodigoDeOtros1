83bec9dd4a72e44d26b735cfd132bd3b6b11ee41
Sebastian Montes de Oca <ing.montesdeoca94@gmail.com>
2023-11-27 15:37:59 -0600

    add: second changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const usersEndpoint = `${baseEndpoint}/users`; // esta linea se puede omitir si se utiliza en la 
+const $n = document.querySelector('.name'); // Se requiere cambiar el selector a uno de tipo clase
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { // Se requiere declarar como una funcion 
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  try { // Se requiere un try - catch
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); // Se agrega esta linea para extraer los datos del cuerpo de la respuesta HTTP como un objeto JS
+    console.log(data);
+    $n.textContent = '${data.name}';
+    $b.textContent = '${data.blog}';
+    $l.textContent = '${data.location}';
+  } catch (err) { // Se adhiere el catch
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`; // hacia falta $ para relacionarlo con un elemento del DOM y ;
 }
 
 displayUser('stolinski').catch(handleError);
